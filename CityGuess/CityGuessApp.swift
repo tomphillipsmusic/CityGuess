6d113920211f854a5daa0c988a3e3f645421886d
Tom Phillips <tomphillipsmusic@gmail.com>
2023-04-06 09:15:01 -0400

    Initial Commit

---
+++ CityGuess/CityGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityGuessApp.swift
+//  CityGuess
+//
+//  Created by Tom Phillips on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CityGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
