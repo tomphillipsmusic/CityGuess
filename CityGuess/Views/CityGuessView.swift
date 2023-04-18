4229fe8998de232e7b2d495bee2c2626087902de
Tom Phillips <tomphillipsmusic@gmail.com>
2023-04-18 10:54:17 -0400

    Got pinch to zoom working on photos. Pinch to zoom is not reset when new picture arrives

--- CityGuess/Views/CityGuessView.swift
+++ CityGuess/Views/CityGuessView.swift
@@ -9,15 +9,13 @@ import SwiftUI
 import CachedAsyncImage
 
 struct CityGuessView<ViewModel: CityGuessViewModel>: View {
-    enum Field {
-        case guess
-    }
     @ObservedObject var vm: ViewModel
     @State private var guess = ""
+    @State var lastScaleValue: CGFloat = 1.0
     @FocusState private var textFieldFocused: Bool
-    @State private var opacity = 1.0
     @State private var autofillSuggestions = [ViewModel.CityModel]()
-    
+    @State private var zoomableScrollView = ZoomableScrollView() {}
+        
     var body: some View {
         
         VStack {
@@ -29,14 +27,15 @@ struct CityGuessView<ViewModel: CityGuessViewModel>: View {
                     .foregroundColor(vm.isCorrect ? .green : .red)
                         
                 CachedAsyncImage(url: URL(string: vm.cityImages[vm.currentCityIndex].url)) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
+                    ZoomableScrollView {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    }
                     
                 } placeholder: {
                     ProgressView()
                 }
-                
                 
                 TextField("Guess", text: $guess)
                     .padding()
@@ -83,16 +82,65 @@ struct CityGuessView<ViewModel: CityGuessViewModel>: View {
         .onChange(of: guess) { guess in
             withAnimation {
                 autofillSuggestions = vm.autofillSuggestions(for: guess)
-                opacity = autofillSuggestions.isEmpty ? 1.0 : 0.2
             }
         }
         
     }
 }
-#warning("Check out Zoe's example of adding autofill for the keyboard. Figure out some kind of animation for showing right or wrong answers")
 
 struct CityGuessView_Previews: PreviewProvider {
     static var previews: some View {
         CityGuessView(vm: TrainingViewModel())
     }
+}
+
+// https://stackoverflow.com/questions/58341820/isnt-there-an-easy-way-to-pinch-to-zoom-in-an-image-in-swiftui
+struct ZoomableScrollView<Content: View>: UIViewRepresentable {
+  private var content: Content
+
+  init(@ViewBuilder content: () -> Content) {
+    self.content = content()
+  }
+
+  func makeUIView(context: Context) -> UIScrollView {
+    // set up the UIScrollView
+    let scrollView = UIScrollView()
+    scrollView.delegate = context.coordinator  // for viewForZooming(in:)
+    scrollView.maximumZoomScale = 20
+    scrollView.minimumZoomScale = 1
+    scrollView.bouncesZoom = true
+
+    // create a UIHostingController to hold our SwiftUI content
+    let hostedView = context.coordinator.hostingController.view!
+    hostedView.translatesAutoresizingMaskIntoConstraints = true
+    hostedView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    hostedView.frame = scrollView.bounds
+    scrollView.addSubview(hostedView)
+
+    return scrollView
+  }
+
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(hostingController: UIHostingController(rootView: self.content))
+  }
+
+  func updateUIView(_ uiView: UIScrollView, context: Context) {
+    // update the hosting controller's SwiftUI content
+    context.coordinator.hostingController.rootView = self.content
+    assert(context.coordinator.hostingController.view.superview == uiView)
+  }
+
+  // MARK: - Coordinator
+
+  class Coordinator: NSObject, UIScrollViewDelegate {
+    var hostingController: UIHostingController<Content>
+
+    init(hostingController: UIHostingController<Content>) {
+      self.hostingController = hostingController
+    }
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+      return hostingController.view
+    }
+  }
 }
