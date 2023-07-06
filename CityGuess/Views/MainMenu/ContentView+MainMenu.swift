ca42f660aa027c90a28beb81956c98039120e635
Tom Phillips <tomphillipsmusic@gmail.com>
2023-07-06 14:49:43 -0400

    Locked Explore Cities view if player has not yet played the game

---
+++ CityGuess/Views/MainMenu/ContentView+MainMenu.swift
@@ -0,0 +1,85 @@
+//
+//  MainMenuView.swift
+//  CityGuess
+//
+//  Created by Tom Phillips on 7/6/23.
+//
+
+import SwiftUI
+
+extension ContentView {
+    var mainMenu: some View {
+        ZStack {
+            if dynamicTypeSize < .accessibility1 {
+                menuBackgroud
+            }
+
+            VStack {
+                dailyChallengeButton
+                trainingButton
+                exploreButton
+
+                Spacer().frame(height: UIScreen.main.bounds.height * 0.4)
+            }
+        }
+        .onReceive(timer, perform: { _ in
+            withAnimation {
+                dailyChallengeViewModel.calculateUnlockProgress()
+            }
+        })
+        .navigationTitle("City Guess")
+    }
+
+    var menuBackgroud: some View {
+        Image("city-skyline-background")
+            .resizable()
+            .scaledToFill()
+            .if(colorScheme == .dark) { view in
+                view.colorInvert()
+                    .opacity(0.8)
+                    .blur(radius: 0.1)
+            }
+    }
+    var dailyChallengeButton: some View {
+        ZStack {
+            MainMenuButton("Daily Challenge") {
+                router.path.append(Router.Screen.challenge)
+            }
+            .disabled(dailyChallengeViewModel.isLocked)
+            .opacity(dailyChallengeViewModel.isLocked ? 0.8 : 1.0)
+
+            if dailyChallengeViewModel.isLocked {
+                LockView(
+                    progress: dailyChallengeViewModel.unlockProgress,
+                    unlockHint: dailyChallengeViewModel.unlockText
+                )
+            }
+
+        }
+    }
+
+    var trainingButton: some View {
+        MainMenuButton("Training") {
+            router.path.append(Router.Screen.training)
+        }
+    }
+
+    var exploreButton: some View {
+        let isLocked = gameHistory.guessHistory.isEmpty
+
+        return ZStack {
+            MainMenuButton("Explore Cities") {
+                router.path.append(Router.Screen.explore)
+            }
+            .disabled(isLocked)
+            .opacity(isLocked ? 0.8 : 1.0)
+
+            if isLocked {
+                LockView(
+                    progress: 0.0,
+                    unlockHint: "This feature unlocks once you have played at least one game of City Guess"
+                )
+            }
+        }
+    }
+}
